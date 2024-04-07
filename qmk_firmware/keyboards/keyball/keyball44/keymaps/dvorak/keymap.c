75a0c7cd530a9ade61fb8f6b060bfc4c00a0f1c4
ywake <wake@drdng.dev>
2024-04-07 14:15:53 +0900

    Refactor: Change width

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/dvorak/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/dvorak/keymap.c
@@ -40,31 +40,31 @@ enum keymap_layer
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [DVOR] = LAYOUT_universal(
-    KC_TAB   , KC_QUOT  , KC_COMM  , KC_DOT   , KC_P     , KC_Y     ,            KC_F     , KC_G     , KC_C     , KC_R     , KC_L     , _______  ,
-    MY_LGUI  , KC_A     , KC_O     , KC_E     , KC_U     , KC_I     ,            KC_D     , KC_H     , KC_T     , KC_N     , KC_S     , KC_MINS  ,
-    KC_LSFT  , KC_SLSH  , KC_Q     , KC_J     , KC_K     , KC_X     ,            KC_B     , KC_M     , KC_W     , KC_V     , KC_Z     , _______  ,
-              MY_LCTL   , KC_LALT  , MY_LNG2  , MY_SPC   , MY_LNG1  ,            MY_ENT   , KC_BSPC  , BALL     , BALL     , TG(QWER)
+    KC_TAB  , KC_QUOT , KC_COMM , KC_DOT  , KC_P    , KC_Y    ,           KC_F    , KC_G    , KC_C    , KC_R    , KC_L    , _______ ,
+    MY_LGUI , KC_A    , KC_O    , KC_E    , KC_U    , KC_I    ,           KC_D    , KC_H    , KC_T    , KC_N    , KC_S    , KC_MINS ,
+    KC_LSFT , KC_SLSH , KC_Q    , KC_J    , KC_K    , KC_X    ,           KC_B    , KC_M    , KC_W    , KC_V    , KC_Z    , _______ ,
+              MY_LCTL , KC_LALT , MY_LNG2 , MY_SPC  , MY_LNG1 ,           MY_ENT  , KC_BSPC , BALL    , BALL    , TG(QWER)
   ),
 
   [QWER] = LAYOUT_universal(
-    _______  , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , _______  ,
-    _______  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,            KC_H     , KC_J     , KC_K     , KC_L     , KC_QUOT  , _______  ,
-    _______  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , _______  ,
-               _______  , _______ , _______  ,  _______  , _______  ,            _______  , _______  , BALL     , BALL     , _______
+    _______ , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,           KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , _______ ,
+    _______ , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,           KC_H    , KC_J    , KC_K    , KC_L    , KC_QUOT , _______ ,
+    _______ , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    ,           KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , _______ ,
+              _______ , _______ , _______ , _______ , _______ ,           _______ , _______ , BALL    , BALL    , _______
   ),
 
   [MOUSE] = LAYOUT_universal(
-    KC_ESC   , KC_F10   , KC_F1    , KC_F2    , KC_F3    , _______  ,            KC_PGUP  , KC_HOME  , KC_UP    , KC_END   , _______  , _______  ,
-    _______  , KC_F11   , KC_F4    , KC_F5    , KC_F6    , _______  ,            KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-    _______  , KC_F12   , KC_F7    , KC_F8    , KC_F9    , _______  ,            KC_BTN4  , KC_BTN1  , KC_BTN2  , KC_BTN3  , KC_BTN5  , _______  ,
-               _______  , _______  , _______  , _______  , _______  ,            _______  , KC_DEL   , BALL     , BALL     , _______
+    KC_ESC  , KC_F10  , KC_F1   , KC_F2   , KC_F3   , _______ ,           KC_PGUP , KC_HOME , KC_UP   , KC_END  , _______ , _______ ,
+    _______ , KC_F11  , KC_F4   , KC_F5   , KC_F6   , _______ ,           KC_PGDN , KC_LEFT , KC_DOWN , KC_RGHT , _______ , _______ ,
+    _______ , KC_F12  , KC_F7   , KC_F8   , KC_F9   , _______ ,           KC_BTN4 , KC_BTN1 , KC_BTN2 , KC_BTN3 , KC_BTN5 , _______ ,
+              _______ , _______ , _______ , _______ , _______ ,           _______ , KC_DEL  , BALL    , BALL    , _______
   ),
 
   [SYMBL] = LAYOUT_universal(
-    _______  , KC_0     , KC_1     , KC_2     , KC_3     , _______  ,            _______  , _______  , KC_LBRC  , KC_RBRC  , _______  , _______  ,
-    _______  , KC_PDOT  , KC_4     , KC_5     , KC_6     , _______  ,            _______  , KC_SCLN  , KC_LPRN  , KC_RPRN  , KC_GRV   , _______  ,
-    _______  , _______  , KC_7     , KC_8     , KC_9     , _______  ,            _______  , KC_ASTR  , KC_EQL   , KC_MINS  , KC_BSLS  , _______  ,
-               _______  , _______  , _______  , _______  , _______  ,            _______  , KC_DEL   , BALL     , BALL     , _______
+    _______ , KC_0    , KC_1    , KC_2    , KC_3    , _______ ,           _______ , _______ , KC_LBRC , KC_RBRC , _______ , _______ ,
+    _______ , KC_PDOT , KC_4    , KC_5    , KC_6    , _______ ,           _______ , KC_SCLN , KC_LPRN , KC_RPRN , KC_GRV  , _______ ,
+    _______ , _______ , KC_7    , KC_8    , KC_9    , _______ ,           _______ , KC_ASTR , KC_EQL  , KC_MINS , KC_BSLS , _______ ,
+              _______ , _______ , _______ , _______ , _______ ,           _______ , KC_DEL  , BALL    , BALL    , _______
   ),
 };
 // clang-format on
