241bcc2a6fb80957cd6e74e3dcc0f62a6fdc0199
ywake <wakey404@gmail.com>
2025-02-23 17:12:30 +0900

    feat: Qwerty layer ではS&SをOFFに

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/dvorak/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/dvorak/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                               KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , _______ ,
     RESERVE , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,                               KC_H    , KC_J    , KC_K    , KC_L    , KC_QUOT , _______ ,
     RESERVE , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    ,                               KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , RESERVE ,
-                        RESERVE , RESERVE , RESERVE , RESERVE , RESERVE ,           RESERVE , KC_BSPC ,           BALL    , BALL    , RESERVE
+                        RESERVE , RESERVE , RESERVE , KC_SPC , RESERVE ,           RESERVE , KC_BSPC ,           BALL    , BALL    , RESERVE
   ),
 
   [MOUSE] = LAYOUT_universal(
